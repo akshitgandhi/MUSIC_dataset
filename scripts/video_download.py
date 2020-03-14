231d79c3e5a1acfcb6dce214801f00ad8d4bd272
Akshit Gandhi <akshitgandhi@Akshits-MacBook.local>
2020-03-14 15:17:21 -0400

    added code for video2image

--- scripts/video_download.py
+++ scripts/video_download.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 import youtube_dl
 import os
 import json
+import cv2
 
 def downloadVideo(url_list, outdir):
 	if not os.path.exists(outdir):
@@ -21,12 +22,31 @@ def main():
 		video_data = json.load(f)
 		data = video_data['videos']
 
+	save_dir = './'
+
 	for key, val in data.iteritems():
 		url_list = []
 		for i in val:
 			url_list.append('https://www.youtube.com/watch?v='+i)
-		print(url_list)
-		downloadVideo(url_list, './'+key.replace(' ', '_'))
+		downloadVideo(url_list, save_dir+key.replace(' ', '_'))
+
+		print('Converting video to frames')
+		for i in val:
+			outdir = save_dir+key.replace(' ', '_')+'/'+i
+			if not os.path.exists(outdir):
+				os.makedirs(outdir)
+			
+			vidcap = cv2.VideoCapture(save_dir+key.replace(' ', '_')+'/'+i+'.mp4')
+			success,image = vidcap.read()
+			count = 0
+			print('[In progress] conversion for %s category, %s video', key, i)
+
+			while success:
+				cv2.imwrite(outdir + '/' + str(count).zfill(6)+'.jpg', image)
+				success,image = vidcap.read()
+				count += 1
+			print('[Done] conversion for %s category, %s video', key, i)
+
 
 
 if __name__ == '__main__':
